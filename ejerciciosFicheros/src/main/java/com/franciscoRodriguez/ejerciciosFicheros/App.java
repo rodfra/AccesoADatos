e9c0001786b54fffaac940ce50bec0dab70eaff1
Francis Rodríguez <rodriguezxf@gmail.com>
2023-09-19 20:02:29 +0200

    Ejercicio0102
    Escribir en ficheros de texto

--- ejerciciosFicheros/src/main/java/com/franciscoRodriguez/ejerciciosFicheros/App.java
+++ ejerciciosFicheros/src/main/java/com/franciscoRodriguez/ejerciciosFicheros/App.java
@@ -1,14 +1,32 @@
 package com.franciscoRodriguez.ejerciciosFicheros;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 // Francisco Rodríguez - 2ºDAM Semipresencial
 
 /**
- * Ejercicio 01.01 - Escribir Hola, desde Eclipse
- * Instala el JDK, Eclipse y crea un programa escriba "Hola" en la pantalla.
+ * Ejercicio 01.02 - Escribir en ficheros de texto
+ * Crea un programa que escriba los números impares del 1 al 10 en un fichero
+ *  de texto, cada uno en una línea.
  */
 
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hola" );
+    	
+		PrintWriter pw = null;
+
+    	try {
+    		pw = new PrintWriter("ejercicio0102.txt");
+    		
+    		for (int i = 1; i <= 9; i+=2) {
+    			pw.println(i);
+    		}
+    		
+    		pw.close();
+    		
+    	} catch (IOException e) {
+    		System.err.println("Error escribiendo: " + e.getMessage());
+    	}
     }
 }
