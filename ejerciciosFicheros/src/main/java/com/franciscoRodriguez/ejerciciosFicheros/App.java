7a96e25f878dbb6bc35b289f111e0cd1e4932044
Francis Rodríguez <rodriguezxf@gmail.com>
2023-09-19 21:12:09 +0200

    Corrigiendo espacios
    Actualización 1.2

--- ejerciciosFicheros/src/main/java/com/franciscoRodriguez/ejerciciosFicheros/App.java
+++ ejerciciosFicheros/src/main/java/com/franciscoRodriguez/ejerciciosFicheros/App.java
@@ -16,40 +16,38 @@ import java.util.Scanner;
 
 public class App {
     public static void main( String[] args ) {
-    	
-    	PrintWriter pw = null;
-    	Scanner sc = new Scanner(System.in);
-    	int alto, ancho;
-    	
-    	try {
-    		pw = new PrintWriter("rectangle.txt");
-    		
-    		System.out.print("Introduce el alto: ");
-    		alto = sc.nextInt();
-    		
-    		System.out.print("Introduce el ancho: ");
-    		ancho = sc.nextInt();
-    		sc.close();
-    		
-    		for (int fila = 0; fila < alto; fila++) {
-    			for (int columna = 0; columna < ancho; columna++) {
-    				if (fila == 0 || columna == 0 || 
-    						fila == alto - 1 || columna == ancho - 1)
-    					pw.print('*');
-    				else
-    					pw.print(' ');
-    			}
-    			
-    			pw.println();
-    		}
-    		
-    	} catch (IOException e) {
-    		System.err.println("Error: " + e.getMessage());
-    		
-    	} finally {
-    		if (pw != null) {
-    			pw.close();
-    		}
+        
+        PrintWriter pw = null;
+        Scanner sc = new Scanner(System.in);
+        int alto, ancho;
+        
+        try {
+            pw = new PrintWriter("rectangle.txt");
+            
+            System.out.print("Introduce el alto: ");
+            alto = sc.nextInt();
+            
+            System.out.print("Introduce el ancho: ");
+            ancho = sc.nextInt();
+            sc.close();
+            
+            for (int fila = 0; fila < alto; fila++) {
+                for (int columna = 0; columna < ancho; columna++) {
+                    if (fila == 0 || columna == 0 || 
+                            fila == alto - 1 || columna == ancho - 1)
+                        pw.print('*');
+                    else
+                        pw.print(' ');
+                }
+                
+                pw.println();
+            }
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        } finally {
+            if (pw != null) {
+                pw.close();
+            }
+        }
     	}
-    }
 }
