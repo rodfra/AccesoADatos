ad4839c32586a13478ec37c2143f7184d70005fd
Francis Rodríguez <rodriguezxf@gmail.com>
2023-09-19 20:49:37 +0200

    Ejercicio 01.04
    Escribir en ficheros de texto + finally

--- ejerciciosFicheros/src/main/java/com/franciscoRodriguez/ejerciciosFicheros/App.java
+++ ejerciciosFicheros/src/main/java/com/franciscoRodriguez/ejerciciosFicheros/App.java
@@ -7,28 +7,47 @@ import java.util.Scanner;
 // Francisco Rodríguez - 2ºDAM Semipresencial
 
 /**
- * Ejercicio 01.03 - Escribir en ficheros de texto + throws
- * Crea un programa que escriba en un fichero ”triangle.txt” un triángulo 
- * creciente de asteriscos, del ancho y alto escogido por el usuario.
+ * Ejercicio 01.04 - Escribir en ficheros de texto + finally
+ * Crea un programa que escriba en un fichero ”rectangle.txt” un rectángulo 
+ * hueco de asteriscos, del ancho y alto escogido por el usuario.
  */
 
 public class App {
-    public static void main( String[] args ) throws IOException {
+    public static void main( String[] args ) {
     	
+    	PrintWriter pw = null;
 		Scanner sc = new Scanner(System.in);
-		PrintWriter pw = new PrintWriter("triangle.txt");
-		int altoYAncho;
-		
-		System.out.print("Introduce el ancho y el alto: ");
-		altoYAncho = sc.nextInt();
-		
-		for (int fila = 0; fila < altoYAncho; fila++) {
-			for (int columna = 0; columna <= fila; columna++) {
-				pw.print('*');
-			}
-			pw.println();
-		}
-		
-		pw.close();
+		int alto, ancho;
+
+    	try {
+        	pw = new PrintWriter("rectangle.txt");
+        	
+    		System.out.print("Introduce el alto: ");
+    		alto = sc.nextInt();
+    		
+    		System.out.print("Introduce el ancho: ");
+    		ancho = sc.nextInt();
+    		sc.close();
+    		
+    		for (int fila = 0; fila < alto; fila++) {
+    			for (int columna = 0; columna < ancho; columna++) {
+    				if (fila == 0 || columna == 0 || 
+    						fila == alto - 1 || columna == ancho - 1)
+    					pw.print('*');
+    				else
+    					pw.print(' ');
+    			}
+    			
+    			pw.println();
+    		}
+    		
+    	} catch (IOException e) {
+    		System.err.println("Error: " + e.getMessage());
+    		
+    	} finally {
+    		if (pw != null) {
+    			pw.close();
+    		}
+    	}
     }
 }
